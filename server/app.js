cb668559210dbda7baa254ef5f553cc561450af1
Phil Curtis <Phil@PhilCurtis.io>
2017-04-10 11:34:11 -0500

    database connected to

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var portDecision = process.env.PORT || 5000;
 
-var mongoConnection = require('./')
+var mongoConnection = require('./modules/mongo-connection')
 
 app.get('/', function(req, res){
   res.sendFile(path.resolve('server/public/views/index.html'));
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 
 app.use(express.static('server/public'));
 
-
+mongoConnection.connect();
 
 
 
