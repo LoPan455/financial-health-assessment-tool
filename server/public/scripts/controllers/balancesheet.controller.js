780d7efabefaee3f6c0c4133b85cc6f4f781256a
Edmund Wippler <ed.wippler@gmail.com>
2017-04-13 16:40:45 -0500

    changed real estate asset section in balance sheet one

--- server/public/scripts/controllers/balancesheet.controller.js
+++ server/public/scripts/controllers/balancesheet.controller.js
@@ -2,4 +2,8 @@ fhatApp.controller('BalanceSheetController', function(ClientFactory) {
   console.log('BalanceSheetController controller running');
   var self = this;
     self.user = ClientFactory.client;
+
+    // test variable
+    // self.user.numberVehicles = 2;
+    // self.user.housing = 'Own';
 });//end app.controller
