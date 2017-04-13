a39486c8f8446557aa2ee34a9fe605602c287d3b
Lopan455 <tjohander@gmail.com>
2017-04-13 11:07:57 -0500

    Merge Issues

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var portDecision = process.env.PORT || 5000;
 var user = require('./routes/user.route.js');
-var client = require('./routes/client.route.js');
+var client = require('./routes/clientSchema.route.js');
 var decoder = require('./modules/decoder.js')
 
 app.get('/', function(req, res){
